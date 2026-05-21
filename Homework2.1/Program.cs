02e2155949c367e62970d6e041ed2495224e89b8
Giorgi8-sichi <giorgi8sichi@gmail.com>
2026-05-21 15:53:47 +0400

    New code (#1)

--- Homework2.1/Program.cs
+++ Homework2.1/Program.cs
@@ -9,9 +9,11 @@ class Program
         Console.WriteLine("Giorgi sichinava");
         Console.ResetColor();
         
+        Console.WriteLine("Enter something: ");
+        var info = Console.ReadLine();
+        Console.WriteLine($"This is user input: {info}");
         
-        
-        
-        
+
+
     }
 }
