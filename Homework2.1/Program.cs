97376361a814017a936e60b067de54579bb7cf4c
Giorgi Sichinava <giorgi8sichi@gmail.com>
2026-05-21 15:42:01 +0400

    Home Work 2

---
+++ Homework2.1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework2._1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.BackgroundColor = ConsoleColor.DarkGray;
+        Console.WriteLine("Giorgi sichinava");
+        Console.ResetColor();
+        
+        Console.WriteLine("Enter something: ");
+        var info = Console.ReadLine();
+        Console.WriteLine(info);
+        
+        
+        
+    }
+}
